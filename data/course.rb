6dc3e11f8a9a30c2a2355afd6fdaed4e43728d90
Franklin Webber <franklin.webber@gmail.com>
2013-02-28 12:31:53 -0700

    Courses can be defined without names in YAML

--- data/course.rb
+++ data/course.rb
@@ -1,7 +1,12 @@
 class Course < Middleman::DataModel::SimpleDataModel
 
   def topics
-    Array(@topics).map do |topic_hash|
+    Array(@topics).map do |subtopic|
+      if subtopic.is_a? String
+        topic_hash = { name: subtopic }
+      else
+        topic_hash = subtopic
+      end
       self.class.new topic_hash
     end
   end
