60ab56d0e3174204b467d91624a13186920f839f
Franklin Webber <franklin.webber@gmail.com>
2013-03-01 09:15:55 -0700

    Footer completely converted over to using YAML data

--- data/contact_point.rb
+++ data/contact_point.rb
@@ -1,7 +1,23 @@
 class ContactPoint < Middleman::DataModel::SimpleDataModel
 
-  def info
+  def self.twitter
+    find_by_type "twitter"
+  end
+
+  def self.github
+    find_by_type "github"
+  end
+
+  def self.find_by_type(type)
+    all.find {|contact| contact.type == type }
+  end
+
+  def tag
     info_text_for_type(type)
+  end
+
+  def url
+    info_url_for_type(type)
   end
 
   private
@@ -9,10 +25,26 @@ class ContactPoint < Middleman::DataModel::SimpleDataModel
   def info_text_for_type(type)
     if type == "twitter"
       "<a href='http://twitter.com/#{@info}'>#{@info}</a>"
+
+    elsif type == "github"
+      "<a href='http://github.com/#{@info}'>#{@info}</a>"
+
     elsif type == "email"
       "<a href='mailto:#{@info}'>#{@info}</a>"
     else
-      "<a href=''>#{@info}</a>"
+      @info
+    end
+  end
+
+  def info_url_for_type(type)
+    if type == "twitter"
+      "http://twitter.com/#{@info}"
+    elsif type == "github"
+      "http://github.com/#{@info}"
+    elsif type == "email"
+      "mailto:#{@info}"
+    else
+      @info
     end
   end
 
